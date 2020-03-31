5efc4c55fbdabf57724e652014562aa30ec35bb1
Mentallsoft <efrain.pizarro@uao.edu.co>
2020-03-31 12:09:11 -0500

    Actualizaciòn de la pagina web, con tema claro y nuevo navbar

---
+++ src/Components/ToOurTeams/Photos.js
@@ -0,0 +1,34 @@
+//Dependences
+import React from 'react'
+
+//Style
+import "./CSS/Photos.css"
+
+class Photos extends React.Component {
+
+    render() {
+        return (
+            <div className="o-PhotoTeam">
+                <div className="ContainerImage">
+                    <img src={this.props.Photo} alt="" />
+                </div>
+
+                <div className="o-ToAnimation">
+                    <div className="o-TeamInformation">
+                        <h5>
+                            {this.props.Member}
+                            <br />
+                            <br />
+                            {this.props.Role}
+                            <br />
+                            <br />
+                            Ext. {this.props.Extension}
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Photos;
